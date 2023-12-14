11750d1dd6ebfc3a5f6135b36a0fdd3bfde1a30e
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-12-14 10:58:49 +0530

    add movies created successfulyy

--- js/index.js
+++ js/index.js
@@ -1,58 +1,26 @@
 import { db } from './firebaseConfig.js';
-import { 
-    addDoc, collection, getDocs, 
-    where, query, deleteDoc,
-    doc, updateDoc 
-} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
-
-// Add note button
-const postNoteButton = document.querySelector('#postNotesButton');
-postNoteButton.addEventListener('click', () => {
-    const note = {
-        username: document.querySelector('#usernamePost').value,
+import {
+    addMovie
+} from './module/add.js';
+// Add movie button
+const addMoviesButton = document.querySelector('#addMoviesButton');
+addMoviesButton.addEventListener('click', () => {
+    const movie = {
         title: document.querySelector('#title').value,
-        note: document.querySelector('#note').value,
-        createdAt: new Date().toLocaleDateString()
+        genre: document.querySelector('#genre').value,
+        releaseDate:  document.querySelector('#releaseDate').value
       }
-
-      postNote(note);
+try {
+    addMovie(movie).then((res)=> {
+        // ON successful addition clear the Dialog
+        document.querySelector('#title').value ="";
+        document.querySelector('#genre').value = "";
+        document.querySelector('#releaseDate').value ="";
+    });
+} catch (error) {
+    console.log(`ERROR: ${error}`);
+}
+      
 });
 
-// add note function
-async function postNote(note) {
-    try {
-        await addDoc(collection(db, 'notes'), note)
-    } catch (error) {
-        console.log(`ERROR: ${error}`);
-    }
-}
-async function getNotes(username) {
-    try {
-        const queryUsername = query(collection(db, 'notes'), where('username', '==', username));
-        const notes = await getDocs(queryUsername);
 
-        const formatedNotes = [];
-
-        notes.forEach((note) => {
-            const formatedNote = {
-                id: note.id,
-                note: note.data()
-            }
-
-            formatedNotes.push(formatedNote);
-        });
-
-        return formatedNotes;
-    } catch (error) {
-        console.log(`ERROR: ${error}`);
-    }
-}
-// To test add note
-// var note={
-//     username : 'Vedika',
-//     title: 'sample',
-//     note: 'nilesh'
-// }
-// postNote(note).then(e => {
-//     console.log("Added successfully");
-// });
---
+++ js/module/add.js
@@ -0,0 +1,18 @@
+import { db } from '../firebaseConfig.js';
+import { 
+    addDoc, collection,
+    
+} from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
+
+
+// add movie function
+async function addMovie(movie) {
+    try {
+        await addDoc(collection(db, 'movies'), movie)
+    } catch (error) {
+        console.log(`ERROR: ${error}`);
+        throw error;
+    }
+}
+
+export{addMovie};
