445fb94b2cf680f7ea96d9c45ffed78b60f7c22e
Deepa Birari <112015631+deepa5053@users.noreply.github.com>
2023-12-15 10:10:55 +0530

    Added delete functionality

--- js/index.js
+++ js/index.js
@@ -1,61 +1,41 @@
-import { db } from './firebaseConfig.js';
-import {
-    addMovie
-} from './module/add.js';
-import {
-    getMovielist
-} from './module/get.js';
+import { db } from "./firebaseConfig.js";
+import { addMovie } from "./module/add.js";
+import { getMovielist, createMovieForm } from "./module/get.js";
+
 // Add movie button
-const addMoviesButton = document.querySelector('#addMoviesButton');
-addMoviesButton.addEventListener('click', () => {
-    const movie = {
-        title: document.querySelector('#title').value,
-        genre: document.querySelector('#genre').value,
-        releaseDate:  document.querySelector('#releaseDate').value
-      }
-try {
-    addMovie(movie).then((res)=> {
-        // ON successful addition clear the Dialog
-        document.querySelector('#title').value ="";
-        document.querySelector('#genre').value = "";
-        document.querySelector('#releaseDate').value ="";
+const addMoviesButton = document.querySelector("#addMoviesButton");
+addMoviesButton.addEventListener("click", () => {
+  const movie = {
+    title: document.querySelector("#title").value,
+    genre: document.querySelector("#genre").value,
+    releaseDate: document.querySelector("#releaseDate").value,
+  };
+  try {
+    addMovie(movie).then((res) => {
+      // ON successful addition clear the Dialog
+      document.querySelector("#title").value = "";
+      document.querySelector("#genre").value = "";
+      document.querySelector("#releaseDate").value = "";
     });
-} catch (error) {
+  } catch (error) {
     console.log(`ERROR: ${error}`);
-}
-      
+  }
 });
 
 // Movie list button event listner
 
-const movieListButton = document.querySelector('#listMovieButton');
-movieListButton.addEventListener('click', () => {
-   try {
-    getMovielist ().then ((movies) => {
-        movies.forEach(doc => {
-            const movie = doc.data();
-           // console.log('title=' + movie.title);
-            formSection.style.display = "flex";
-  movieSection.style.display = "none";
-
-  // Create a form dynamically
-  const formElem = document.createElement("form");
-  formElem.setAttribute("id", "movieForm");
-
-  // Create input elements for form
-
-  const titleElem = document.createElement("input");
-
-  titleElem.setAttribute("type", "text");
-  // titleElem.setAttribute("required", "required");
-  //titleElem.required = true;
-  titleElem.setAttribute("placeholder", "Title:");
-  titleElem.setAttribute("id", "movie-title");
-        });
+const movieListButton = document.querySelector("#listMovieButton");
+movieListButton.addEventListener("click", () => {
+  try {
+    getMovielist().then((movies) => {
+      movies.forEach((doc) => {
+        const movie = doc.data();
+        console.log ("docid="+ doc.id);
+        // console.log('title=' + movie.title);
+        createMovieForm(document, doc.id, movie);
+      });
     });
-    
-   } catch (error) {
+  } catch (error) {
     console.log(`ERROR: ${error}`);
-   }
-    
+  }
 });
--- js/module/get.js
+++ js/module/get.js
@@ -1,6 +1,6 @@
 import { db } from '../firebaseConfig.js';
 import { 
-    getDocs, collection,
+    getDocs, collection, deleteDoc,doc
     
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
@@ -11,5 +11,101 @@ async function getMovielist () {
     return movies;
 
 }
+// Function for deleting movie
+async function deleteMovie(id) {
+    try {
+      await deleteDoc(doc(db, "movies", id)).then (()=> {
+        console.log("Deleted doc id successfully " + id);
+      });
+    } catch (error) {
+      console.log(`ERROR: ${error}`);
+    }
+  }
 
-export {getMovielist};
+//function for form
+    const formSection = document.querySelector("#form");
+    const movieSection = document.querySelector("#movieList");
+    const confirmationSection = document.querySelector("#confirmation");
+function createMovieForm(document, docid, movie){
+    
+
+ 
+    document.getElementById("listMovieButton").innerText = "See MovieList";
+    //headSection.style.display = "none";
+    formSection.style.display = "flex";
+    movieSection.style.display = "none";
+  
+    //dynamic form
+    const formElem = document.createElement("form");
+    formElem.setAttribute("id", "movieForm");
+  
+    // input elements for form
+  
+    const titleElem = document.createElement("input");
+  
+    titleElem.setAttribute("type", "text");
+    titleElem.value=movie.title;
+    
+    // titleElem.setAttribute("required", "required");
+    titleElem.required = true;
+    titleElem.setAttribute("placeholder", "Title:");
+    titleElem.setAttribute("id", "movie-title");
+    titleElem.addEventListener("keyup", async (e) => {
+      console.log(e.target.value);
+      const movieL = await checkTitle(e.target.value);
+      if (movieL.empty) {
+        console.log("empty");
+        deleteButton.removeAttribute("disabled");
+      }
+    });
+  
+    const genreElem = document.createElement("input");
+    genreElem.value=movie.genre;
+    genreElem.setAttribute("placeholder", "Genre:");
+    //genreElem.setAttribute("id", "movie-genre");
+    genreElem.setAttribute("id", "genre");
+  
+    const releaseDateElem = document.createElement("input");
+    
+    releaseDateElem.setAttribute("type", "text");
+    releaseDateElem.setAttribute("placeholder", "Release Date:");
+    releaseDateElem.value=movie.releaseDate;
+    releaseDateElem.setAttribute("id", "releaseDate");
+  
+    const deleteButton = document.createElement("input");
+    deleteButton.setAttribute("type", "button");
+    //deleteButton.setAttribute("id", "submitFormBtn");
+    deleteButton.setAttribute("docid", docid);
+    deleteButton.value = "Delete";
+    
+    deleteButton.addEventListener("click", () => {
+        try {
+          console.log("Deleting docid="+docid);
+          deleteMovie(docid);
+        } catch (error) {
+          console.log(`ERROR: ${error}`);
+        }
+      });
+    formElem.append(titleElem);
+    formElem.append(genreElem);
+    formElem.append(releaseDateElem);
+    formElem.append(deleteButton);
+  
+    formSection.append(formElem);
+  }
+  
+  function successConfirmation(title) {
+    confirmationSection.innerHTML = "";
+    formSection.style.display = "none";
+    confirmationSection.style.display = "block";
+    const titleElem = document.createElement("p");
+    titleElem.setAttribute("id", "confirm-msg");
+    titleElem.innerText =
+      title.charAt(0).toUpperCase() + title.substring(1) + " Added Successfully";
+  
+  
+    confirmationSection.append(titleElem);
+    // successSection.append(backButtonElem);
+}
+
+export {getMovielist, createMovieForm};
